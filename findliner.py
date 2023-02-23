3ad3faf326f29fdda92c24f3f7299565b0cb8e45
Zilin Jiang <libragold@gmail.com>
2023-02-23 09:40:09 -0700

    v2

--- findliner.py
+++ findliner.py
@@ -1,83 +1,89 @@
-import os, glob
 from sys import argv
+import pathlib, tempfile, subprocess, io
+from pypdf import PdfReader, PdfWriter
+from reportlab.pdfgen import canvas as reportlab_canvas
 
 font_size = 6
+baseline_shift = 2
 offset_top = int(argv[2]) if len(argv) > 2 else 0
 offset_bottom = int(argv[3]) if len(argv) > 3 else 0
 margin_left = int(argv[4]) if len(argv) > 4 else 35
 
-from pathlib import Path
-
-filename = Path(argv[1]).stem
-file_extension = Path(argv[1]).suffix
+file = pathlib.Path(argv[1])
+filename = pathlib.Path(argv[1]).stem
+file_extension = pathlib.Path(argv[1]).suffix.lower()
 
 if file_extension != '.pdf':
-    print('Cannot open PDF. This file type ({}) is not supported.'.format(file_extension))
+    print('Can open only PDF. This file type ({}) is not supported.'.format(file_extension))
     quit()
 
-from PyPDF2 import PdfReader
-
 # Read your existing PDF
-existing_pdf = PdfReader(open("{}.pdf".format(filename), "rb"))
+existing_pdf = PdfReader(file)
 
 number_of_pages = len(existing_pdf.pages)
-lines = []
+lines_per_page = []
+png_heights = []
 
-import tempfile
+with tempfile.TemporaryDirectory() as temp_dir:
+    png_fn = str(pathlib.Path(temp_dir)/'pngs')
+    pdf_fn = str(pathlib.Path(temp_dir)/'pdf')
 
-with tempfile.TemporaryDirectory() as tempdirname:
+    # Repair potentially corrupted PDF
+    # https://superuser.com/questions/278562/how-can-i-fix-repair-a-corrupted-pdf-file/282056#282056
+    subprocess.run(['gs', '-o', pdf_fn, '-sDEVICE=pdfwrite', '-dPDFSETTINGS=/prepress', file])
+    
+    # Reread your existing PDF
+    existing_pdf = PdfReader(pdf_fn)
 
-    # print(tempdirname)
-    tempfilename = str(Path(tempdirname)/filename)
-    png_filename = tempfilename+"-0" if number_of_pages == 1 else tempfilename # when pdf consists of a single page, imagemagick does not append a counter to the png file
+    # When pdf consists of a single page, imagemagick does not append a counter to the png file
+    png_fn_alt = png_fn + '-0' if number_of_pages == 1 else png_fn
+    subprocess.run(['convert', pdf_fn, '-strip', 'png:{}'.format(png_fn_alt)])
 
-    cmd = '''convert {}.pdf -strip png:{}
-    for f in {}-*; do
-        convert $f -flatten -resize 1X1000! -black-threshold 99% -white-threshold 10% -negate -morphology Erode Diamond -morphology Thinning:-1 Skeleton -black-threshold 50% txt:-| sed -e '1d' -e '/#000000/d' -e 's/^[^,]*,//' -e 's/[(]//g' -e 's/:.*//' -e 's/,/ /g' > $f.txt;
-    done'''.format(filename, png_filename, tempfilename)
+    for idx in range(number_of_pages):
+        print('Processing {}/{}...'.format(idx+1, number_of_pages))
+        dimension = subprocess.run(['identify', '-ping', '-format', "'%w %h'", '{}-{}'.format(png_fn, idx)], capture_output=True, encoding='utf8').stdout[1:-1]
+        height = int(dimension.split()[1])
+        png_heights.append(height)
+        blob = subprocess.run(['convert', '{}-{}'.format(png_fn, idx), '-flatten', '-resize', '1X{}!'.format(height), '-black-threshold', '99%', '-white-threshold', '10%', '-negate', '-morphology', 'Erode', 'Diamond', '-morphology', 'Thinning:-1', 'Skeleton', '-black-threshold', '50%', 'txt:-'], capture_output=True, encoding='utf8').stdout
+        lines = []
+        for line in blob.splitlines()[1:]:
+            if not('#0000000' in line):
+                lines.append(int(line.split(':')[0].split(',')[1]))
+        lines_per_page.append(lines[offset_top:len(lines)-offset_bottom])
 
-    os.system(cmd)
-
-    for i in range(number_of_pages):
-        with open('{}-{}.txt'.format(tempfilename, i)) as file:
-            ys = [int(line) for line in file.readlines()]
-            lines.append(ys[offset_top:len(ys)-offset_bottom])
-
-from PyPDF2 import PdfWriter
-import io
-from reportlab.pdfgen import canvas
-from reportlab.lib import colors
+## Add text to existing PDF using Python
+# https://stackoverflow.com/questions/1180115/add-text-to-existing-pdf-using-python
 
 # Create a new PDF with Reportlab
 packet = io.BytesIO()
-can = canvas.Canvas(packet)
+canvas = reportlab_canvas.Canvas(packet)
 
-for i in range(number_of_pages):
-    page = existing_pdf.pages[i]
+for idx, lines in enumerate(lines_per_page):
+    page = existing_pdf.pages[idx]
     width = page.mediabox[2]
     height = page.mediabox[3]
-    can.setPageSize((width, height))
-    can.setFillColor(colors.red)
-    can.setFont('Courier', font_size)
-    line = lines[i]
-    for j in range(len(line)):
-        can.drawString(margin_left, int(height - height * line[j] / 1000) - font_size / 3, '{}.{}'.format(i+1, j+1))
-    can.showPage()
+    canvas.setPageSize((width, height))
+    canvas.setFont('Courier', font_size)
+    for j, y in enumerate(lines):
+        canvas.drawString(margin_left, int(height - y * height / png_heights[idx]) - baseline_shift, '{}.{}'.format(idx+1, j+1))
+    canvas.showPage()
 
-can.save()
+canvas.save()
 
 # Move to the beginning of the StringIO buffer
 packet.seek(0)
+
+# Create a new PDF with Reportlab
 new_pdf = PdfReader(packet)
 output = PdfWriter()
 
-# Add the "watermark" (which is the new pdf) on the existing page
-for i in range(number_of_pages):
-    page = existing_pdf.pages[i]
-    page.merge_page(new_pdf.pages[i])
+# Add the line numbers (which is the new pdf) on the existing page
+for idx in range(number_of_pages):
+    page = existing_pdf.pages[idx]
+    page.merge_page(new_pdf.pages[idx])
     output.add_page(page)
 
 # Finally, write "output" to a real file
-outputStream = open('{}-with-line-numbers-old.pdf'.format(filename), 'wb')
+outputStream = open(file.parents[0]/'{}-with-line-numbers.pdf'.format(filename), 'wb')
 output.write(outputStream)
 outputStream.close()
